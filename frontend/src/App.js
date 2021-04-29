667b9944c5399c48a980b97e3ea85715d778ceb4
mxbialostocki <matt.bialostocki@gmail.com>
2021-04-29 17:29:37 +1200

    react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,24 @@
 import { Container } from 'react-bootstrap'
+import { BrowserRouter as Router, Route} from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
 
 import HomePage from './pages/HomePage'
+import ProductPage from './pages/ProductPage'
 
 const App = () => {
   return (
-    <div className="App">
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
           <h1>sick bookshop grrl</h1>
-          <HomePage />
+          <Route path='/' component={HomePage} exact />
+          <Route path='/product/:id' component={ProductPage} />
         </Container>
       </main>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
---
+++ frontend/src/pages/ProductPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProductPage = () => {
+    return (
+        <h1>heyheyhey</h1>
+    )
+}
+
+export default ProductPage
